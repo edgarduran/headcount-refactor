fd834495db98d4253bfdbdbec07317a9dab8c27e
John Slota <john.slota@gmail.com>
2015-10-07 16:41:16 -0400

    state_wide added

--- test/test_economicprofile.rb
+++ test/test_economicprofile.rb
@@ -6,12 +6,12 @@ require 'economicprofile'
 class TestEconomicProfile < Minitest::Test
 
 
-  def free_or_reduced_lunch_in_year(year)
+  def test_free_or_reduced_lunch_in_year
     path       = File.expand_path("../data", __dir__)
     repository = DistrictRepository.from_csv(path)
     district   = repository.find_by_name("ACADEMY 20")
     expected = {2010 => 0.895, 2011 => 0.895, 2012 => 0.889, 2013 => 0.913, 2014 => 0.898}
-    assert_equal expected, district.economicprofile.graduation_rate_by_year
+    assert_equal expected, district.economicprofile.free_or_reduced_lunch_in_year(2010)
   end
 
   def test_first
