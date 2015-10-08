4723292ffc66a4eb8c214de23a978f5c38d3a14e
Edgar Duran <edgarduran@Edgars-MacBook-Pro.local>
2015-10-08 10:23:34 -0600

    merging and stuff

--- lib/parser.rb
+++ lib/parser.rb
@@ -21,16 +21,9 @@ class Parse
     CSV.read(fullpath, headers: true, header_converters: :symbol)
   end
 
-  # def path
-  #   path = File.expand_path("../data", __dir__)
-  # end
-
   def get_a_hash(rows)
     big_hash = rows.map {|array| array.to_h}
     big_hash.select{|row| row.fetch(:location).upcase == @name.upcase}
   end
 
-
 end
-h = {:location=>"ACADEMY 20", :category=>"All Students", :timeframe=>"2011", :dataformat=>"Percent", :data=>"0.002"}
-h[:category]
