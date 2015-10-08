b870fb15a6e925e6aeec2dcbdb7ca3de8a5cd76a
John Slota <john.slota@gmail.com>
2015-10-08 11:49:39 -0400

    refactored statewide testing

--- lib/statewide_testing.rb
+++ lib/statewide_testing.rb
@@ -11,11 +11,7 @@ class StatewideTesting < AllCsvFiles
 
   def proficient_by_grade(grade)
     raise UnknownDataError unless valid_grades?(grade)
-    if grade == 3
-      csv_file = THIRD_GRADE
-    elsif grade == 8
-      csv_file = EIGHTH_GRADE
-    end
+    csv_file = get_csv_from_grade(grade)
     parsed = send_to_parser(csv_file)
     data = parsed.select { |row| row if row.fetch(:dataformat) == "Percent" }
     time = data.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
@@ -26,21 +22,53 @@ class StatewideTesting < AllCsvFiles
     parsed_math    = send_to_parser(MATH)
     parsed_reading = send_to_parser(READING)
     parsed_writing = send_to_parser(WRITING)
+    data = parsed_math.select { |row| row if row.fetch(:dataformat) == "Percent" }
+    time = data.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
+  end
 
+  def proficient_for_subject_by_grade_in_year(subject, grade, year)
+    raise UnknownDataError unless valid_subjects?(subject)
+    csv_file = get_csv_from_grade(grade)
+    parsed = send_to_parser(csv_file)
+    data = parsed.select { |row| row if row.fetch(:score) == subject.to_s.capitalize && row.fetch(:dataformat) == "Percent" }
+    time = data.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
+    time.fetch(year)
+  end
+
+  def proficient_for_subject_by_race_in_year(subject, race, year)
+    raise UnknownDataError unless valid_subjects?(subject)
+    csv_file = get_csv_from_subject(subject)
+    parsed = send_to_parser(csv_file)
+    data = parsed.select { |row| row if row.fetch(:dataformat) == "Percent" && row.fetch(:race_ethnicity) == race.to_s.capitalize }
+    time = data.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
+    time.fetch(year)
   end
 
   def proficient_for_subject_in_year(subject, year)
-    if subject    == :math
-      csv_file    = MATH
-   elsif subject  == :reading
-      csv_file    = READING
-   elsif subject  == :writing
-      csv_file    = WRITING
-    end
+    raise UnknownDataError unless valid_subjects?(subject)
+    csv_file = get_csv_from_subject(subject)
     parsed = send_to_parser(csv_file)
     data = parsed.select { |row| row if row.fetch(:dataformat) == "Percent" }
     time = data.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
     time.fetch(year)
+  end
+
+  def get_csv_from_grade(grade)
+    if grade == 3
+      csv_file = THIRD_GRADE
+    elsif grade == 8
+      csv_file = EIGHTH_GRADE
+    end
+  end
+
+  def get_csv_from_subject(subject)
+    if subject    == :math
+      csv_file    = MATH
+    elsif subject  == :reading
+      csv_file    = READING
+    elsif subject  == :writing
+      csv_file    = WRITING
+    end
   end
 
   def valid_grades?(grade)
@@ -50,6 +78,10 @@ class StatewideTesting < AllCsvFiles
   def valid_races?(race)
     ["asian", "black", "pacific Islander", "hispanic",
      "native american", "two or more", "white"]
+  end
+
+  def valid_subjects?(subjects)
+    ["math", "reading", "writing"]
   end
 
   class UnknownDataError < StandardError
--- test/test_statewide_testing.rb
+++ test/test_statewide_testing.rb
@@ -13,13 +13,29 @@ class TestStatewideTesting < Minitest::Test
     assert_equal expected, district.statewide_testing.proficient_by_grade(3)
   end
 
-  def test_proficient_by_race_or_ethnicity(race_input)
+  def test_proficient_by_race_or_ethnicity
     skip
     path       = File.expand_path("../data", __dir__)
     repository = DistrictRepository.from_csv(path)
     district   = repository.find_by_name("ACADEMY 20")
     expected   = {2008=>0.671, 2009=>0.706, 2010=>0.662, 2011=>0.678, 2012=>0.655, 2013=>0.668, 2014=>0.639}
-    assert_equal expected, district.proficient_by_race_or_ethnicity(race_input)
+    assert_equal expected, district.statewide_testing.proficient_by_race_or_ethnicity("white")
+  end
+
+  def test_proficient_for_subject_by_grade_in_year
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("ACADEMY 20")
+    expected   = 0.83
+    assert_equal expected, district.statewide_testing.proficient_for_subject_by_grade_in_year(:math, 3, 2012)
+  end
+
+  def test_proficient_for_subject_by_race_in_year
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("ACADEMY 20")
+    expected   = 0.818
+    assert_equal expected, district.statewide_testing.proficient_for_subject_by_race_in_year(:math, :asian, 2012)
   end
 
   def test_proficient_for_subject_in_year
