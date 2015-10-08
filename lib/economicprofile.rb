e7e639e5430e9e4141b7935f9b1c0463b5bc2d77
John Slota <john.slota@gmail.com>
2015-10-08 10:37:23 -0400

    econ profile done

--- lib/economicprofile.rb
+++ lib/economicprofile.rb
@@ -12,16 +12,35 @@ class EconomicProfile < AllCsvFiles
   def free_or_reduced_lunch_by_year
     csv_file = REDUCED_LUNCH
     parsed = send_to_parser(csv_file)
-    time = parsed.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
-    binding.pry
+    data = parsed.select { |row| row if row.fetch(:dataformat) == "Percent" &&
+                                        row.fetch(:poverty_level) == "Eligible for Free or Reduced Lunch" }
+    time = data.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
   end
 
   def free_or_reduced_lunch_in_year(year)
-    csv_file = REDUCED_LUNCH
+    free_or_reduced_lunch_by_year.fetch(year)
+  end
+
+  def school_aged_children_in_poverty_by_year
+    csv_file = POVERTY
     parsed = send_to_parser(csv_file)
-    time = parsed.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
-    binding.pry
-    time.fetch(year)
+    data = parsed.select { |row| row if row.fetch(:dataformat) == "Percent" }
+    time = data.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
+  end
+
+  def school_aged_children_in_poverty_in_year(year)
+    school_aged_children_in_poverty_by_year.fetch(year)
+  end
+
+  def title_1_students_by_year
+    csv_file = TITLE_1
+    parsed = send_to_parser(csv_file)
+    data = parsed.select { |row| row if row.fetch(:dataformat) == "Percent" }
+    time = data.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
+  end
+
+  def title_1_students_in_year(year)
+    title_1_students_by_year.fetch(year)
   end
 
   def graduation_rate_by_year
