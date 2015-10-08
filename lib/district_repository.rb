cc02812e4053fc3d4c5ab2d59495af6ab3340275
Edgar Duran <edgarduran@Edgars-MacBook-Pro.local>
2015-10-07 22:19:21 -0600

    even more methods done

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -56,33 +56,88 @@ class Enrollment < AllCsvFiles
   end
 
   def dropout_rate_by_gender_in_year(year_input)
-  if year_input.to_s.length != 4
-    return nil
+    if year_input.to_s.length != 4
+      return nil
+    end
+    csv_file = DROPOUT
+    parsed = send_to_parser(csv_file)
+    line = {}
+    hash = {}
+    parsed.each do |columns|
+      district  = columns[:location]
+      category  = columns[:category]
+      year      = columns[:timeframe]
+      stat_type = columns[:dataformat]
+      value     = columns[:data]
+      if district == "ACADEMY 20"
+        if year == year_input.to_s && category == "Female Students" || year == year_input.to_s && category == "Male Students"
+          if category == "Female Students"
+            category = category[0..5].downcase
+          elsif
+            category == "Male Students"
+            category = category [0..3].downcase
+          end
+          hash = Hash[category.to_sym, (value.to_f * 1000).to_i / 1000.0]
+          line = line.merge(hash)
+        end
+      end
+    end
+    return line
   end
-  csv_file = DROPOUT
-  parsed = send_to_parser(csv_file)
-  parsed.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
-  # data.each do |columns|
-  #   district  = columns[:location]
-  #   category  = columns[:category]
-  #   year      = columns[:timeframe]
-  #   stat_type = columns[:dataformat]
-  #   value     = columns[:data]
-  #   if district == "ACADEMY 20"
-  #     if year == year_input.to_s && category == "Female Students" || year == year_input.to_s && category == "Male Students"
-  #       if category == "Female Students"
-  #         category = category[0..5].downcase
-  #       elsif
-  #         category == "Male Students"
-  #         category = category [0..3].downcase
-  #       end
-  #       hash = Hash[category.to_sym, (value.to_f * 1000).to_i / 1000.0]
-  #       line = line.merge(hash)
-  #     end
-  #   end
-  # end
-  # return line
-end
+
+  def dropout_rate_by_race_in_year(year_input)
+    if year_input.to_s.length != 4
+      return nil
+    end
+    data = CSV.open "#{@path}/Dropout rates by race and ethnicity.csv", headers: true, header_converters: :symbol
+    line = {}
+    hash = {}
+    data.each do |columns|
+      district  = columns[:location]
+      category  = columns[:category]
+      year      = columns[:timeframe]
+      stat_type = columns[:dataformat]
+      value     = columns[:data]
+      if district == "ACADEMY 20"
+        if category == "Asian Students" || category == "Black Students" || category == "Native Hawaiian or Other Pacific Islander" || category == "Hispanic Students" || category == "Native American Students" || category == "Two or More Races" || category == "White Students"
+          if category == "Asian Students"
+            category = category[0..4].downcase
+          elsif
+            category == "Black Students"
+            category = category[0..4].downcase
+          elsif
+            category == "Native Hawaiian or Other Pacific Islander"
+            category = "pacific_islander"
+          elsif
+            category == "Hispanic Students"
+            category = category[0..7].downcase
+          elsif
+            category == "Native American Students"
+            category = "native_american"
+          elsif
+            category == "Two or More Races"
+            category = "two_or_more"
+          elsif
+            category == "White Students"
+            category = category[0..4].downcase
+          end
+          hash = Hash[category.to_sym, (value.to_f * 1000).to_i / 1000.0]
+          line = line.merge(hash)
+        end
+      end
+    end
+    return line
+  end
+
+
+  def dropout_rate_for_race_or_ethnicity(race)
+
+  end
+
+  def dropout_rate_for_race_or_ethnicity_in_year(race, year_input)
+
+  end
+
 
   def graduation_rate_by_year
     csv_file = HS_GRAD_RATES
@@ -90,8 +145,26 @@ end
     parsed.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
   end
 
-  def graduation_rate_in_year(year)
+  def graduation_rate_in_year(year_input)
+    if year_input.to_s.length != 4
+      return nil
+    end
     time = graduation_rate_by_year
-    time.fetch(year)
+    time.fetch(year_input)
   end
+
+  def kindergarten_participation_by_year
+    csv_file = K_FULL_DAY
+    parsed = send_to_parser(csv_file)
+    parsed.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
+  end
+
+  def kindergarten_participation_in_year(year_input)
+    if year_input.to_s.length != 4
+      return nil
+    end
+    time = kindergarten_participation_by_year
+    time.fetch(year_input)
+  end
+
 end
--- lib/test_district_repository.rb
+++ lib/test_district_repository.rb
@@ -34,4 +34,51 @@ class TestEnrollment < Minitest::Test
     assert_equal expected, district.enrollment.dropout_rate_by_gender_in_year(2011)
   end
 
+  def test_graduation_rates_by_year
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("ACADEMY 20")
+    expected = {2010 => 0.895, 2011 => 0.895, 2012 => 0.889, 2013 => 0.913, 2014 => 0.898}
+    assert_equal expected, district.enrollment.graduation_rate_by_year
+  end
+
+  def test_graduation_rates_in_year
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("ACADEMY 20")
+    assert_equal 0.895, district.enrollment.graduation_rate_in_year(2010)
+  end
+
+  def test_unknown_years_return_nil
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("Colorado")
+    assert_equal nil, district.enrollment.graduation_rate_in_year(232323322332)
+  end
+
+  def test_returns_a_hash_of_years_to_truncated_kindergarten_participation_percentages
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("Colorado")
+    expected = {2007=>0.394, 2006=>0.336, 2005=>0.278, 2004=>0.24, 2008=>0.535,
+                2009=>0.598, 2010=>0.64, 2011=>0.672, 2012=>0.695, 2013=>0.702,
+                2014=>0.741}
+    assert_equal expected, district.enrollment.kindergarten_participation_by_year
+  end
+
+  def test_unknown_years_return_nil
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("ACADEMY 20")
+    assert_equal nil, district.enrollment.kindergarten_participation_in_year(232323322332)
+  end
+
+  def kindergarten_participation_in_year
+    path       = File.expand_path("../data", __dir__)
+    repository = DistrictRepository.from_csv(path)
+    district   = repository.find_by_name("Colorado")
+    binding.pry 
+    assert_equal 0.436, district.enrollment.kindergarten_participation_in_year(2010)
+  end
+
 end
