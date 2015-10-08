de4622fccfe15097185de03efe34b8a856613bfa
John Slota <john.slota@gmail.com>
2015-10-08 10:38:18 -0400

    removed unneeded tests

--- lib/economicprofile.rb
+++ lib/economicprofile.rb
@@ -43,14 +43,4 @@ class EconomicProfile < AllCsvFiles
     title_1_students_by_year.fetch(year)
   end
 
-  def graduation_rate_by_year
-    csv_file = HS_GRAD_RATES
-    parsed = send_to_parser(csv_file)
-    parsed.map { |row| [row.fetch(:timeframe).to_i, (row.fetch(:data).to_f * 1000).to_i/ 1000.0] }.to_h
-  end
-
-  def graduation_rate_in_year(year)
-    time = graduation_rate_by_year
-    time.fetch(year)
-  end
 end
--- test/test_economicprofile.rb
+++ test/test_economicprofile.rb
@@ -53,18 +53,4 @@ class TestEconomicProfile < Minitest::Test
     assert_equal expected, district.economicprofile.title_1_students_in_year(2009)
   end
 
-  def test_first
-    path       = File.expand_path("../data", __dir__)
-    repository = DistrictRepository.from_csv(path)
-    district   = repository.find_by_name("ACADEMY 20")
-    assert_equal 0.895, district.economicprofile.graduation_rate_in_year(2010)
-  end
-
-  def test_second
-    path       = File.expand_path("../data", __dir__)
-    repository = DistrictRepository.from_csv(path)
-    district   = repository.find_by_name("ACADEMY 20")
-    expected = {2010 => 0.895, 2011 => 0.895, 2012 => 0.889, 2013 => 0.913, 2014 => 0.898}
-    assert_equal expected, district.economicprofile.graduation_rate_by_year
-  end
 end
